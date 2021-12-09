c3795d1db34679ef1f1656a82823dde625ba820d
Tamine Mélissa <melissa.tamine@ensae.fr>
2021-12-10 00:41:14 +0100

    modify utils.py, mt5_generator.py

--- src/data/utils.py
+++ src/data/utils.py
@@ -26,3 +26,7 @@ def split_into_train_test_validation_sets(questions: List[Question],
 def yield_batches(input_list: List[object], batch_size=12) -> List[object]:
     for i in tqdm(range(0, len(input_list), batch_size)):
         yield input_list[i: i + batch_size]
+
+
+
+
