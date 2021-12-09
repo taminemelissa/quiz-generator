1e5ea63d83fe3836d43fbc965bd07e9749d1e9d9
Tamine Mélissa <melissa.tamine@ensae.fr>
2021-12-09 15:56:45 +0100

    add a function in utils.py

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,4 +1,6 @@
 from data.data_format import *
+import random as rd
+from tqdm import tqdm
 
 
 def split_into_train_test_validation_sets(questions: List[Question],
@@ -8,7 +10,7 @@ def split_into_train_test_validation_sets(questions: List[Question],
     train_count = int(train_ratio * total)
     test_count = int(test_ratio * total)
     if total - train_count - test_count > 0:
-        random.shuffle(questions)
+        rd.shuffle(questions)
         train_questions = questions[0:train_count]
         test_questions = questions[train_count:train_count + test_count]
         dev_questions = questions[train_count + test_count:]
@@ -19,3 +21,8 @@ def split_into_train_test_validation_sets(questions: List[Question],
         }
     else:
         raise Exception('Set correct ratios')
+
+
+def yield_batches(input_list: List[object], batch_size=12) -> List[object]:
+    for i in tqdm(range(0, len(input_list), batch_size)):
+        yield input_list[i: i + batch_size]
