56c708d77a3d25d058717fbb692a019b5204c2fc
Tamine Mélissa <melissa.tamine@ensae.fr>
2021-12-14 07:52:54 +0100

    modify data_format.py and utils.py

--- src/data/data_format.py
+++ src/data/data_format.py
@@ -1,6 +1,4 @@
-import json
 from typing import Dict, List, Any, OrderedDict, Union
-import numpy as np
 import collections
 
 
@@ -15,12 +13,10 @@ class BaseItem(object):
     def __init__(self, text: str = None,
                  identifier: str = None,
                  scores: OrderedDict[str, float] = None,
-                 vector: np.ndarray = None,
                  meta: Dict[str, Any] = None):
         self.text = text
         self.identifier = identifier
         self.scores = scores
-        self.vector = vector
         self.meta = meta
 
 
@@ -29,16 +25,14 @@ class Context(BaseItem):
                  title: str = None,
                  identifier: str = None,
                  scores: OrderedDict[str, float] = None,
-                 vector: np.ndarray = None,
                  meta: Dict[str, Any] = None):
         """
         :param text: the text of the context
         :param identifier: the identifier of the context
         :param scores: the score dictionary of the context
-        :param vector: the vector representation of the context
         :param meta: the meta information
         """
-        super(Context, self).__init__(text=text, identifier=identifier, scores=scores, vector=vector, meta=meta)
+        super(Context, self).__init__(text=text, identifier=identifier, scores=scores, meta=meta)
         self.title = title
 
 
@@ -47,30 +41,16 @@ class Answer(BaseItem):
                  context: Context = None,
                  identifier: str = None,
                  scores: OrderedDict[str, float] = None,
-                 vector: np.ndarray = None,
-                 meta: Dict[str, Any] = None,
-                 start_char_position: int = -1,
-                 end_char_position: int = -1,
-                 start_token_position: int = -1,
-                 end_token_position: int = -1
+                 meta: Dict[str, Any] = None
                  ):
         """
         :param text: the text of the answer
         :param context: the context from which the answer is extracted
         :param identifier: the identifier of the context
         :param scores: the score dictionary of the context
-        :param vector: the vector representation of the context
         :param meta: the meta information
-        :param start_char_position: the position of the first character of the answer into the context
-        :param end_char_position: the position of the last character of the answer into the context
-        :param start_token_position: the position of the first token of the answer into the (tokenized) context
-        :param end_token_position: the position of the last token of the answer into the (tokenized) context
         """
-        super(Answer, self).__init__(text=text, identifier=identifier, scores=scores, vector=vector, meta=meta)
-        self.start_char_position = start_char_position
-        self.end_char_position = end_char_position
-        self.start_token_position = start_token_position
-        self.end_token_position = end_token_position
+        super(Answer, self).__init__(text=text, identifier=identifier, scores=scores, meta=meta)
         self.context = context
 
 
@@ -78,7 +58,6 @@ class Question(BaseItem):
     def __init__(self, text: str = None,
                  identifier: str = None,
                  scores: OrderedDict[str, float] = None,
-                 vector: np.ndarray = None,
                  meta: Dict[str, Any] = None,
                  retrieved_contexts: List[Context] = None,
                  gold_answers: List[Answer] = None,
@@ -88,13 +67,12 @@ class Question(BaseItem):
         :param text: the text of the question
         :param identifier: the identifier of the question
         :param scores: the score dictionary of the question
-        :param vector: the vector representation of the question
         :param retrieved_contexts: the retrieved documents as contexts for this question, if a retriever is involved
         :param meta: the meta information
         :param gold_answers: the gold answers of the question
         :param predicted_answers: the predicted answers of the question
         """
-        super(Question, self).__init__(text=text, identifier=identifier, scores=scores, vector=vector, meta=meta)
+        super(Question, self).__init__(text=text, identifier=identifier, scores=scores, meta=meta)
         self.gold_answers = gold_answers
         self.retrieved_contexts = retrieved_contexts
         self.predicted_answers = predicted_answers
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,32 +1,8 @@
-from data.data_format import *
-import random as rd
+from typing import List
 from tqdm import tqdm
-
-
-def split_into_train_test_validation_sets(questions: List[Question],
-                                          train_ratio: float = 0.7,
-                                          test_ratio: float = 0.2) -> dict:
-    total = len(questions)
-    train_count = int(train_ratio * total)
-    test_count = int(test_ratio * total)
-    if total - train_count - test_count > 0:
-        rd.shuffle(questions)
-        train_questions = questions[0:train_count]
-        test_questions = questions[train_count:train_count + test_count]
-        dev_questions = questions[train_count + test_count:]
-        return {
-            'train': train_questions,
-            'test': test_questions,
-            'dev': dev_questions
-        }
-    else:
-        raise Exception('Set correct ratios')
 
 
 def yield_batches(input_list: List[object], batch_size=12) -> List[object]:
     for i in tqdm(range(0, len(input_list), batch_size)):
         yield input_list[i: i + batch_size]
-
-
-
 
